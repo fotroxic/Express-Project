2587794b1026589b9acf5d85b2913fd2ef16a68d
Onur Ago <fotroxic@gmail.com>
2022-06-17 21:44:54 +0200

    Updated

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mysql = require("mysql");
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env' });
+const db = require('./model/db');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 var app = express();
+
+db.start.connect(function (err) {
+  if (err) {
+    console.log('Error connecting to the database');
+  } else {
+    console.log('Connected to MYSQL');
+  }
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,16 +34,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', require('./routes/auth'));
+
+
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
