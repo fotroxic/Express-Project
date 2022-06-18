beeab8dce2dbb2f28925ab5856cb2e9e67eb8835
Onur Ago <fotroxic@gmail.com>
2022-06-18 03:01:27 +0200

    added nodemailer'

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mysql = require("mysql");
+const dotenv = require('dotenv');
+const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+
+
+
+
+dotenv.config({ path: './.env' });
+const db = require('./model/db');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 var app = express();
+
+db.start.connect(function (err) {
+  if (err) {
+    console.log('Error connecting to the database');
+  } else {
+    console.log('Connected to MYSQL');
+  }
+});
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,17 +40,72 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json({ extended: false }));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', require('./routes/auth'));
+
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+const redirect_uri = process.env.REDIRECT_URI;
+const r_token = process.env.REFRESH_TOKEN;
+
+
+async function mainMail(email) {
+
+  const oAut2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uri);
+  oAut2Client.setCredentials({ refresh_token: r_token });
+  const accessToken = oAut2Client.getAccessToken();
+  const transporter = await nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'oAuth2',
+      user: 'fotroxic@gmail.com',
+      clientId: client_id,
+      clientSecret: client_secret,
+      refreshToken: r_token,
+      accessToken: accessToken
+    }
+
+  });
+
+
+  let info = transporter.sendMail({
+    from: 'YOUTUBE PREMIUM <onuragoo@gmail.com>',
+    to: email,
+    subject: 'Join to YOUTUBE PREMIUM',
+    text: 'DENEMEEEE premium'
+  });
+
+};
+
+
+app.post("/mail", async (req, res, next) => {
+  const { ypemail } = req.body;
+  try {
+
+    await mainMail(ypemail);
+
+
+    res.send("Message Successfully Sent!");
+  } catch (error) {
+    console.log(error);
+    res.send("Message Could not be Sent");
+  }
+});
+
+
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,114 @@
 var express = require('express');
 var router = express.Router();
+var authController = require('../controllers/auth');
+const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Onur Ago Express' });
+router.get('/', authController.isLoggedIn, function (req, res, next) {
+
+  let videos = [
+    {
+      channelIcon: '',
+      title: 'Video Title 1',
+      channelName: 'Channel Name1',
+      views: '',
+      date: '',
+
+    },
+
+    {
+      channelIcon: '',
+      title: 'Video Title 1',
+      channelName: 'Channel Name1',
+      views: '',
+      date: '',
+
+    },
+    {
+      channelIcon: '',
+      title: 'Video Title 1',
+      channelName: 'Channel Name1',
+      views: '',
+      date: '',
+
+    },
+    {
+      channelIcon: '',
+      title: 'Video Title 1',
+      channelName: 'Channel Name1',
+      views: '',
+      date: '',
+
+    },
+    {
+      channelIcon: '',
+      title: 'Video Title 1',
+      channelName: 'Channel Name1',
+      views: '',
+      date: '',
+
+    },
+    {
+      channelIcon: '',
+      title: 'Video Title 1',
+      channelName: 'Channel Name1',
+      views: '',
+      date: '',
+
+    },
+    {
+      channelIcon: '',
+      title: 'Video Title 1',
+      channelName: 'Channel Name1',
+      views: '',
+      date: '',
+
+    },
+  ];
+
+  console.log(req.user);
+  res.render('index', {
+    title: 'Onur Ago Express',
+    videos: videos,
+    user: req.user,
+
+  });
+
+
+
+
 });
+
+router.get('/about', function (req, res, next) {
+  res.render('./pages/about', { title: 'About Us' });
+});
+
+router.get('/contact', function (req, res, next) {
+  res.render('./pages/contact', { title: 'Contact' });
+});
+
+
+
+
+
+router.get('/login', function (req, res, next) {
+  let message;
+  res.render('login', { message: message });
+});
+
+router.get('/register', function (req, res, next) {
+  let message;
+  res.render('register', { message: message });
+});
+
+
+
+
+
+
+
+
 
 module.exports = router;
